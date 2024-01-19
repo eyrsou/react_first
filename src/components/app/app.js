70b3412d9d08bdb856125258e4778c42e83a9b66
Viktoria <iamviktrols@yandex.com>
2024-01-20 00:42:52 +0300

    add structure

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+ import './app.css';
+import AppInfo from '../app-info/app-info'
+
+ function App() {
+    return (
+        <div className="app">
+            <AppInfo/>
+            <div className="search-panel">
+                
+            </div>
+        </div>
+    );
+ }
+
+ export default App;
